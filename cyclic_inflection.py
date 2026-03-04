6cc0ae2bc5c6f359d5826c9290d8be66aa232b3a
MCGallaspy <gallaspy.michael@gmail.com>
2026-03-03 18:10:19 -0800

    Refactor form and content models into diff archs

--- cyclic_inflection.py
+++ cyclic_inflection.py
@@ -27,38 +27,62 @@ root_df = pd.DataFrame(data=data)
 train_df = pd.concat([train_df, root_df], ignore_index=True)
 print("Num train rows:", train_df.shape[0])
 
-onegram_vocab = set()
+form_unigram_vocab = set()
 for row in train_df.itertuples():
-    onegram_vocab |= set(row.content)
-    onegram_vocab |= set(row.form)
+    form_unigram_vocab |= set(row.form)
 GLOSS_START, GLOSS_END = "GLOSS_START", "GLOSS_END"
-if len(onegram_vocab) % 2:
-    onegram_vocab |= set((GLOSS_START, GLOSS_END, "OOV", "PAD", "FILL"))
+if len(form_unigram_vocab) % 2:
+    form_unigram_vocab |= set((GLOSS_START, GLOSS_END, "OOV", "PAD", "FILL"))
 else:
-    onegram_vocab |= set((GLOSS_START, GLOSS_END, "OOV", "PAD"))
+    form_unigram_vocab |= set((GLOSS_START, GLOSS_END, "OOV", "PAD"))
 
-onegram_vocab = dict((w, i) for (i, w) in enumerate(onegram_vocab))
-print("Unigram vocab:", onegram_vocab)
-print("Len unigram vocab:", len(onegram_vocab))
+form_unigram_vocab = dict((w, i) for (i, w) in enumerate(form_unigram_vocab))
+print("Form unigram vocab:", form_unigram_vocab)
+print("Len form unigram vocab:", len(form_unigram_vocab))
 
-def get_sequence(word):
+content_unigram_vocab = set()
+for row in train_df.itertuples():
+    content_unigram_vocab |= set(row.content.split(";"))
+if len(content_unigram_vocab) % 2:
+    content_unigram_vocab |= set(("OOV",))
+else:
+    content_unigram_vocab |= set(("OOV", "FILL"))
+
+content_unigram_vocab = dict((w, i) for (i, w) in enumerate(content_unigram_vocab))
+print("Content unigram vocab:", content_unigram_vocab)
+print("Len content unigram vocab:", len(content_unigram_vocab))
+
+def get_form_sequence(word):
     idxs = []
     for c in word:
         try:
-            idxs.append(onegram_vocab[c])
+            idxs.append(form_unigram_vocab[c])
         except KeyError:
-            idxs.append(onegram_vocab["OOV"])
-    idxs = [onegram_vocab[GLOSS_START]] + idxs + [onegram_vocab[GLOSS_END]]
-    return torch.stack([torch.Tensor([int(i == j) for i in range(len(onegram_vocab))]) for j in idxs])
+            idxs.append(form_unigram_vocab["OOV"])
+    idxs = [form_unigram_vocab[GLOSS_START]] + idxs + [form_unigram_vocab[GLOSS_END]]
+    return torch.stack([torch.Tensor([int(i == j) for i in range(len(form_unigram_vocab))]) for j in idxs])
+
+
+def get_content_tensor(content):
+    idxs = []
+    contents = content.split(";")
+    for c in contents:
+        try:
+            idxs.append(content_unigram_vocab[c])
+        except KeyError:
+            idxs.append(content_unigram_vocab["OOV"])
+    idxs = set(idxs)
+    return torch.Tensor([1 if i in idxs else 0 for i in range(len(content_unigram_vocab))])
+
     
-train_df['content_sequence'] = train_df.content.apply(get_sequence)
-train_df['form_sequence'] = train_df.form.apply(get_sequence)
+train_df['content_tensor'] = train_df.content.apply(get_content_tensor)
+train_df['form_sequence'] = train_df.form.apply(get_form_sequence)
 
 test_df = pd.read_csv('part1/data/nav.tst', sep='\t', header=None, names=["root", "content", "form"])
 test_df = test_df.sample(frac=SAMPLE_RATE)
 print("Num test rows:", test_df.shape[0])
-test_df['content_sequence'] = test_df.content.apply(get_sequence)
-test_df['form_sequence'] = test_df.form.apply(get_sequence)
+test_df['content_tensor'] = test_df.content.apply(get_content_tensor)
+test_df['form_sequence'] = test_df.form.apply(get_form_sequence)
 
 dev_df = pd.read_csv('part1/data/nav.dev', sep='\t', header=None, names=["root", "content", "form"])
 dev_df = dev_df.sample(frac=SAMPLE_RATE)
@@ -74,11 +98,11 @@ root_df = pd.DataFrame(data=data)
 dev_df = pd.concat([dev_df, root_df], ignore_index=True)
 print("Num dev rows:", dev_df.shape[0])
 
-dev_df['content_sequence'] = dev_df.content.apply(get_sequence)
-dev_df['form_sequence'] = dev_df.form.apply(get_sequence)
+dev_df['content_tensor'] = dev_df.content.apply(get_content_tensor)
+dev_df['form_sequence'] = dev_df.form.apply(get_form_sequence)
 
 
-class MyModel(torch.nn.Module):
+class FormModel(torch.nn.Module):
 
     def __init__(self, d_input, num_glosses):
         super().__init__()
@@ -121,10 +145,49 @@ class MyModel(torch.nn.Module):
         out = self.log_softmax(transformer_out)
         return out
 
-d_model = len(onegram_vocab)
-print(f"d_model={d_model}")
-form_model = MyModel(d_model*3, d_model)
-content_model = MyModel(d_model*3, d_model)
+
+d_form = len(form_unigram_vocab)
+d_content = len(content_unigram_vocab)
+
+form_model = FormModel(d_form + 2 * d_content, d_form)
+
+
+class ContentModel(torch.nn.Module):
+
+    def __init__(self, d_input, num_labels):
+        super().__init__()
+        self.conv1 = torch.nn.Conv1d(
+            in_channels=d_input,
+            out_channels=256,
+            kernel_size=3,
+            padding='same',
+        )
+        self.conv2 = torch.nn.Conv1d(
+            in_channels=256,
+            out_channels=256,
+            kernel_size=3,
+            padding='same',
+        )
+        self.conv3 = torch.nn.Conv1d(
+            in_channels=256,
+            out_channels=2,
+            kernel_size=3,
+            padding='same',
+        )
+        self.avg_pooling = nn.AdaptiveAvgPool1d(num_labels)
+        self.log_softmax = nn.LogSoftmax(dim=0)
+
+    def forward(self, X):
+        X = torch.permute(X.unsqueeze(0), dims=(0, 2, 1)) # To N, C, L
+        X = self.conv1(X)
+        X = self.conv2(X)
+        X = self.conv3(X)
+        X = self.avg_pooling(X)
+        out = self.log_softmax(X)
+        return out
+
+
+content_model = ContentModel(d_content + 2 * d_form, d_content)
 
 num_params = sum(p.numel() for p in form_model.parameters() if p.requires_grad)
 num_params += sum(p.numel() for p in content_model.parameters() if p.requires_grad)
@@ -140,10 +203,10 @@ def get_random_pair(row, source_df):
     return source_df[mask].sample(n=1).iloc[0]
 
 
-pad_vector = torch.Tensor([int(i == onegram_vocab['PAD']) for i in range(len(onegram_vocab))])
+pad_vector = torch.Tensor([int(i == form_unigram_vocab['PAD']) for i in range(len(form_unigram_vocab))])
 pad_vector = pad_vector.unsqueeze(0)
 
-start_vector = torch.Tensor([int(i == onegram_vocab[GLOSS_START]) for i in range(len(onegram_vocab))])
+start_vector = torch.Tensor([int(i == form_unigram_vocab[GLOSS_START]) for i in range(len(form_unigram_vocab))])
 start_vector = start_vector.unsqueeze(0)
 
 
@@ -156,7 +219,7 @@ def train_step(form_model, content_model, train_df, num_examples=1, num_epochs=1
             pair = get_random_pair(row, train_df)
             
             # First content prediction
-            seqs = (row.form_sequence, row.content_sequence, pair.form_sequence)
+            seqs = (row.form_sequence, pair.form_sequence)
             maxlen = max(s.shape[0] for s in seqs)
             vs = []
             for s in seqs:
@@ -164,35 +227,33 @@ def train_step(form_model, content_model, train_df, num_examples=1, num_epochs=1
                     s = torch.cat((s, pad_vector), dim=0)
                 vs.append(s)
             X = torch.cat(vs, dim=1)
-            y = pair.content_sequence[:-1, :]
-            tgt_mask = nn.Transformer.generate_square_subsequent_mask(y.shape[0])
-            pred_content = content_model(X, y, tgt_mask=tgt_mask, tgt_is_causal=True)
-            actual_content = torch.argmax(pair.content_sequence[1:], axis=1)
-            loss = criterion(pred_content, actual_content)
+            expanded_content = row.content_tensor.expand(maxlen, -1)
+            X = torch.cat([X, expanded_content], dim=1)
+            pred_content = content_model(X)
+            loss = criterion(pred_content, pair.content_tensor.unsqueeze(0).to(torch.long))
 
             # First form prediction
+            maxlen = row.form_sequence.shape[0]
             if forced:
-                seqs = (row.form_sequence, row.content_sequence, pair.content_sequence)
+                expanded_row_content = row.content_tensor.expand(maxlen, -1)
+                expanded_pair_content = pair.content_tensor.expand(maxlen, -1)
+                X = torch.cat([row.form_sequence, expanded_row_content, expanded_pair_content], dim=1)
             else:
-                seqs = (row.form_sequence, row.content_sequence, torch.cat((start_vector, pred_content)))
-            maxlen = max(s.shape[0] for s in seqs)
-            vs = []
-            for s in seqs:
-                while s.shape[0] < maxlen:
-                    s = torch.cat((s, pad_vector), dim=0)
-                vs.append(s)
-            X = torch.cat(vs, dim=1)
+                expanded_row_content = row.content_tensor.expand(maxlen, -1)
+                pred_content = torch.argmax(pred_content.squeeze(), dim=0)
+                expanded_pair_content = pred_content.expand(maxlen, -1)
+                X = torch.cat([row.form_sequence, expanded_row_content, expanded_pair_content], dim=1)
             y = pair.form_sequence[:-1, :]
             tgt_mask = nn.Transformer.generate_square_subsequent_mask(y.shape[0])
             pred_form = form_model(X, y, tgt_mask=tgt_mask, tgt_is_causal=True)
             actual_form = torch.argmax(pair.form_sequence[1:], axis=1)
             loss += criterion(pred_form, actual_form)
-
+            
             # Second content prediction
             if forced:
-                seqs = (pair.form_sequence, pair.content_sequence, row.form_sequence)
+                seqs = (pair.form_sequence, row.form_sequence)
             else:
-                seqs = (torch.cat((start_vector, pred_form)), pair.content_sequence, row.form_sequence)
+                seqs = (torch.cat((start_vector, pred_form)), row.form_sequence)
             maxlen = max(s.shape[0] for s in seqs)
             vs = []
             for s in seqs:
@@ -200,24 +261,22 @@ def train_step(form_model, content_model, train_df, num_examples=1, num_epochs=1
                     s = torch.cat((s, pad_vector), dim=0)
                 vs.append(s)
             X = torch.cat(vs, dim=1)
-            y = row.content_sequence[:-1, :]
-            tgt_mask = nn.Transformer.generate_square_subsequent_mask(y.shape[0])
-            pred_content = content_model(X, y, tgt_mask=tgt_mask, tgt_is_causal=True)
-            actual_content = torch.argmax(row.content_sequence[1:], axis=1)
-            loss += criterion(pred_content, actual_content)
-
+            expanded_content = pair.content_tensor.expand(maxlen, -1)
+            X = torch.cat([X, expanded_content], dim=1)
+            pred_content = content_model(X)
+            loss += criterion(pred_content, row.content_tensor.unsqueeze(0).to(torch.long))
+            
             # Second form prediction
+            maxlen = pair.form_sequence.shape[0]
             if forced:
-                seqs = (pair.form_sequence, pair.content_sequence, row.content_sequence)
+                expanded_row_content = row.content_tensor.expand(maxlen, -1)
+                expanded_pair_content = pair.content_tensor.expand(maxlen, -1)
+                X = torch.cat([pair.form_sequence, expanded_pair_content, expanded_row_content], dim=1)
             else:
-                seqs = (pair.form_sequence, pair.content_sequence, torch.cat((start_vector, pred_content)))
-            maxlen = max(s.shape[0] for s in seqs)
-            vs = []
-            for s in seqs:
-                while s.shape[0] < maxlen:
-                    s = torch.cat((s, pad_vector), dim=0)
-                vs.append(s)
-            X = torch.cat(vs, dim=1)
+                expanded_pair_content = pair.content_tensor.expand(maxlen, -1)
+                pred_content = torch.argmax(pred_content.squeeze(), dim=0)
+                expanded_row_content = pred_content.expand(maxlen, -1)
+                X = torch.cat([pair.form_sequence, expanded_pair_content, expanded_row_content], dim=1)
             y = row.form_sequence[:-1, :]
             tgt_mask = nn.Transformer.generate_square_subsequent_mask(y.shape[0])
             pred_form = form_model(X, y, tgt_mask=tgt_mask, tgt_is_causal=True)
@@ -236,23 +295,20 @@ def train_step(form_model, content_model, train_df, num_examples=1, num_epochs=1
 
 def eval_inflection(form_model, row, eval_df):
     form_model.eval()
-    seqs = (get_sequence(row.root), get_sequence("ROOT"), row.content_sequence)
-    maxlen = max(s.shape[0] for s in seqs)
-    vs = []
-    for s in seqs:
-        while s.shape[0] < maxlen:
-            s = torch.cat((s, pad_vector), dim=0)
-        vs.append(s)
-    X = torch.cat(vs, dim=1)
-    y = torch.zeros((1, len(onegram_vocab)))
-    y[0, onegram_vocab['GLOSS_START']] = 1
+    root_form_sequence = get_form_sequence(row.root)
+    maxlen = root_form_sequence.shape[0]
+    expanded_row_content = row.content_tensor.expand(maxlen, -1)
+    expanded_root_content = get_content_tensor("ROOT").expand(maxlen, -1)
+    X = torch.cat([root_form_sequence, expanded_root_content, expanded_row_content], dim=1)
+    y = torch.zeros((1, len(form_unigram_vocab)))
+    y[0, form_unigram_vocab['GLOSS_START']] = 1
     while True:
         pred = form_model(X, y)
         next_y_label = torch.argmax(pred[-1, :])
         next_y = torch.zeros((1, y.shape[1]))
         next_y[0, next_y_label] = 1
         y = torch.concat([y, next_y], axis=0)
-        if torch.argmax(y[-1, :len(onegram_vocab)]) == onegram_vocab[GLOSS_END]: break
+        if torch.argmax(y[-1, :len(form_unigram_vocab)]) == form_unigram_vocab[GLOSS_END]: break
         if y.shape[0] > 100: break
     return y
 
@@ -260,7 +316,7 @@ def eval_inflection(form_model, row, eval_df):
 def decode_pred(pred):
      return ''.join([g
          for seq in pred[1:-1]
-         for (g, i) in onegram_vocab.items()
+         for (g, i) in form_unigram_vocab.items()
          if i == torch.argmax(seq).item()])
 
 train_losses = []
